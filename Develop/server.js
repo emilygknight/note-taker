1e31c6d08b82a4b704ea00cbb6ee00d7ea6817f3
emilygknight <emilyknight819@gmail.com>
2023-10-29 20:34:13 -0500

    got the notes page to load up

--- Develop/server.js
+++ Develop/server.js
@@ -13,17 +13,27 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-// GET Route for homepage
+// Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'puclic/notes.html'))
-);
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.post('/api/notes', (req,res) => {
+  console.log(`${req.method} request recieved`);
+
+  const { title, text, id } = req.body;
+})
 
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
+
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'notes.html'));
+});
