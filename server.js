a928688e66bd17545a29ef7d8b8cf909726b1180
emilygknight <emilyknight819@gmail.com>
2023-10-30 23:04:08 -0500

    added star instead of slant

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-// Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
@@ -85,6 +82,9 @@ app.post('/api/notes', (req,res) => {
   }
 });
 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
