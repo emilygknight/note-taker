06b09e53edd32210d80edd603640103ea8d728cd
emilygknight <emilyknight819@gmail.com>
2023-10-30 22:56:20 -0500

    fixing syntax

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const notes = require('./Develop/db/db.json');
+const notes = require('./db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
 // const api = require('./public/assets/js/index');
